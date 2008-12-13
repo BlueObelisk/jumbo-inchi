d42ad81bf95cf402cb728bfccdc4368150f0e3c8
petermr <733515+petermr@users.noreply.github.com>
2008-12-13 16:26:09 +0000

    Refactored InChI library out of jumbo as it causes too much trouble. Now loaded at run-time

---
+++ src/main/java/org/xmlcml/cml/inchi/Problems.java
@@ -0,0 +1,13 @@
+package org.xmlcml.cml.inchi;
+
+/**
+ * Enumeration of problems that arise during pre-processing before calculating
+ * an InChI.
+ * 
+ * @author Jim Downing
+ * @since 5.4
+ */
+public enum Problems {
+    /** dewisott */
+    BOND_ORDER
+}
